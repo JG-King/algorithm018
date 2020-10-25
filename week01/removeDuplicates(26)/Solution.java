7964f3562fd3c5178fef51175afddeec960383b4
lg <2431609956@qq.com>
2020-10-25 11:52:35 +0800

    删除排序数组中的重复项(26)

---
+++ week01/removeDuplicates(26)/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+           if(i != j){
+               if(nums[i] != nums[j]){
+                   nums[j+1] = nums[i];
+                     j++;
+               }
+             
+           }
+        }
+        return j + 1;
+    }
+}
