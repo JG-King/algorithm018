0c8862117bf7b2dac727c0e90a631021d53aa5a8
lg <2431609956@qq.com>
2020-11-08 22:34:41 +0800

    全排列

---
+++ week03/permute(46)/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * 全排列
+     * https://leetcode-cn.com/problems/permutations/submissions/
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
+        }
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            Collections.swap(output, first, i);
+            backtrack(n, output, res, first + 1);
+            Collections.swap(output, first, i);
+        }
+    }
+}
