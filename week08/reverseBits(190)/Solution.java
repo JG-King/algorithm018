cec083d26fef6412d9fcbb27c3cc1a4d37f250f5
lg <2431609956@qq.com>
2020-12-13 21:25:57 +0800

    颠倒二进制位

---
+++ week08/reverseBits(190)/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    /**
+     * 颠倒二进制位
+     * https://leetcode-cn.com/problems/reverse-bits/
+     * @param n
+     * @return
+     */
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            res = (res << 1) + (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
